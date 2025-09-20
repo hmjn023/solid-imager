a68773ba61a471bbbb8994aafb970c8498c52a2f
hmjn023 <hmjn023@gmail.com>
2025-09-20 20:59:47 +0900

    uuid

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { v4 as uuidv4 } from "uuid";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
